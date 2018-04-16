6774d4ec5616e7770ca457a649bf8cf1720a10b8
David Neal <35005080+davidpneal@users.noreply.github.com>
2018-04-16 16:41:28 -0500

    Begin adding Pester tests

--- Format-UserGroups/Format-UserGroups.Tests.ps1
+++ Format-UserGroups/Format-UserGroups.Tests.ps1
@@ -1,9 +1,32 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+﻿#4/16/2018
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "Format-UserGroups" {
-    It "does something useful" {
-        $true | Should Be $false
+	
+	#Mock Get-Module{ return $true }
+	#Note: as of April 2018, mocking Get-Module will fail: https://github.com/pester/Pester/issues/1007
+	
+	Mock Get-ADUser{
+		return @{'Name'='John Smith';
+				 'MemberOf'='{CN=EmailUsers,OU=Distribution Lists,DC=domain,DC=com, CN=Domain Users,OU=Security Groups,DC=domain,DC=com}'}
+	}
+
+    It "calls Get-ADUser once per user" {
+        Format-UserGroups -identity dneal, jsmith		
+		Assert-MockCalled Get-ADuser -Times 2 -Exactly -Scope It
     }
+	
+	It "should output one string per user" {
+        Format-UserGroups -identity dneal, jsmith		
+		#test?
+    } -skip
+	
+	It "should accept input via the pipeline" {
+		Get-ADUser jsmith | Format-UserGroups |
+		Should -Not -BeNullOrEmpty
+	}
+	
 }
