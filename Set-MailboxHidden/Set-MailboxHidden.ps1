8bf07321c3b6f14c8da1523748e1ffa40cbd7056
David Neal <35005080+davidpneal@users.noreply.github.com>
2018-05-01 13:13:18 -0500

    Add help to Set-MailboxHidden

--- Set-MailboxHidden/Set-MailboxHidden.ps1
+++ Set-MailboxHidden/Set-MailboxHidden.ps1
@@ -1,7 +1,45 @@
-﻿#4/25/2018
+﻿#5/1/2018
 
 function Set-MailboxHidden {
+	
+	<#
+	.SYNOPSIS
+		This command can be used to hide or unhide a mailbox from the global address list.
+	.DESCRIPTION
+		This command can be used to hide or unhide a mailbox from the global address list in the Office 365 
+		exchange tenant.  This setting is made in the On-Premises Exchange Management Shell and requires 
+		the Set-RemoteMailbox cmdlet to be available.  The cmdlet can either be loaded before running 
+		Set-MailboxHidden or can be loaded from a PSSession that is passed to the command.
+	.PARAMETER identity
+		One or more user account names to hide or unhide from the global address list.  This tool will accept
+		either a SAM account name or a UPN.
+	.PARAMETER session
+		This parameter can be used to pass a previously established PSSession (connected to the On-Prem hybrid 
+		exchange server) to the Set-MailboxHidden tool.  Using this parameter has the advantage that the tool 
+		will load just the single Set-RemoteMailbox command it needs then will unload it afterward.  Note that 
+		this functionality will automatically call Import-PSSession with the -allowclobber flag set.
+	.PARAMETER unhide
+		The default behavior of the tool is to hide the mailbox.  Use this switch to unhide the mailbox.  Note
+		that if more than one identity is provided, the unhide action will be applied to all users in the call. 
+	.EXAMPLE
+		Set-MailboxHidden -identity user 
+		Typical usage - hides the mailbox for the specified user, the OnPrem EMS mailbox command 
+		Set-RemoteMailbox must already be loaded into the PowerShell session
+	.EXAMPLE
+		Set-MailboxHidden -identity user, user1
+		Hides the mailbox for user and user1
+	.EXAMPLE
+		Set-MailboxHidden -identity user -unhide
+		Unhides the specified mailbox
+	.EXAMPLE
+		Get-ADUser jsmith | Set-MailboxHidden
+		Example usage for pipeline input
+	.EXAMPLE
+		Set-MailboxHidden -identity user -PSSession $session
+		Hides the mailbox using the specified PSSession connection to the on-prem EMS
+	#>
 
+	
 	[CmdletBinding(SupportsShouldProcess,
 				   ConfirmImpact='Low')]
 
@@ -36,6 +74,7 @@ function Set-MailboxHidden {
 		
 		#Make sure the Set-RemoteMailbox command is loaded
 		try {
+			##Might need to supress the output from this command - use: > $null
 			get-command "Set-RemoteMailbox" -erroraction stop
 		} catch {
 			write-verbose $_
