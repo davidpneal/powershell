a72ae77050b63d416e11b61ed886767e729a69c5
David Neal <35005080+davidpneal@users.noreply.github.com>
2018-05-11 13:18:54 -0500

    Fix Get-ADUser proxy function throwing the original error at the end of execution
    Note: this code is a disaster and needs to be cleaned up.  Previously, when calling "get-aduser @PSBoundParameters" the original error message generated by Get-ADUser would still be displayed even if the new call with the truncated identity worked OK.  This was being thrown in the End try-catch block (presumabily generated by the $steppablePipeline.End() call).  Various methods of trying to clear the error didn’t work but duplicating the begin block code and calling Get-ADUser that way fixes this problem for some reason.  Seems hackish but has the intended effect.  Will need to verify through testing this doesn’t cause any adverse side effects.

--- ProxyFunctions/Get-ADUserProxy.ps1
+++ ProxyFunctions/Get-ADUserProxy.ps1
@@ -1,4 +1,4 @@
-#5/10/2018
+#5/11/2018
 #The purpose of this proxy function is to help deal with old SAM account names that were truncated at 8 characters
 
 #The plan:
@@ -45,20 +45,24 @@ function Get-ADUser {
 				$PSBoundParameters['OutBuffer'] = 1
 			}
 			$wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('ActiveDirectory\Get-ADUser', [System.Management.Automation.CommandTypes]::Cmdlet)
-			$scriptCmd = {& $wrappedCmd @PSBoundParameters }
+			$scriptCmd = {& $wrappedCmd @PSBoundParameters}
 			$steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
 			$steppablePipeline.Begin($PSCmdlet)
 		} catch {
+			write-output "1 begin catch block"
 			throw
 		}
+		write-output "2 begin block end"
 	}
 
 	process
 	{
+		write-output "3 process block start"
 		try {
 			$steppablePipeline.Process($_)
 		} catch {
 			
+						
 			#Get the string value of the identity key from PSBoundParameters
 			$val = $PSBoundParameters['identity'].tostring()
 			
@@ -67,36 +71,69 @@ function Get-ADUser {
 			
 			
 			#Update the identity key value
-			#Fix - one of these commands displays 'True' to the console
-			$PSBoundParameters.Remove('identity')
-			$PSBoundParameters.Add('identity', $val2)
+			#The .remove method will output 'True' when called, redirect this to $null
+			$PSBoundParameters.Remove('identity') > $null
+			$PSBoundParameters.Add('identity', $val2) 
 			
 			#verify (testing)
 			$newval = $PSBoundParameters['identity'].tostring()
 			
-			write-output "val = $val"
-			write-output "newval = $newval"
+			#write-output "val = $val"
+			#write-output "newval = $newval"
 			
 			
 			#try again - not sure if this is a valid way of doing this - might break the pipeline
 			#Note - this works and gets the truncated identity but still throws the original error 
 			Write-warning "$val not found, trying again with $val2"
 			try {
-				get-aduser @PSBoundParameters
+				write-output "4 calling get-aduser again"
+				#get-aduser @PSBoundParameters
+				try {
+					$outBuffer = $null
+					if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+					{
+						$PSBoundParameters['OutBuffer'] = 1
+					}
+					$wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('ActiveDirectory\Get-ADUser', [System.Management.Automation.CommandTypes]::Cmdlet)
+					$scriptCmd = {& $wrappedCmd @PSBoundParameters}
+					$steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+					$steppablePipeline.Begin($PSCmdlet)
+				} catch {
+					write-output "99 begin catch block"
+					throw
+				}
 			} catch {
+				write-output "5 catch from re-call"
 				throw
 			}
 			
+			write-output "6 catch from process block - end"
 		}
+		write-output "7 end of process block"
+		
 	}
+
 
 	end
 	{
+		write-output "8 end block start"
+		
 		try {
+			#$global:error.clear()
+			#write-output "end block error count: $error.count"
+			#write-output "end block error 0: $error[0]"
+			#write-output "end block  global error 0: $global:error[0]"
+			write-output "9 calling the end steppablePipeline"
 			$steppablePipeline.End()
+			write-output "10 after calling the end steppablePipeline"
 		} catch {
+			write-output "11 catch from end block"
 			throw
+			#THIS is where the throw message from the original error comes from -> $steppablePipeline.End() generates an error when called
+			#this probably isnt valid to comment out - seems to go into an infinite re-call loop without it (TEST MORE)
+			
 		}
+		write-output "12 end of end block"
 	}
 	<#
 
