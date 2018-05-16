850c44e00dbfe39a497cdc95119d7dc6ed947a7a
David Neal <35005080+davidpneal@users.noreply.github.com>
2018-05-16 16:09:24 -0500

    Add code from Set-MailboxHidden to handle some of the basic checks

--- Set-MailboxDelegation/Set-MailboxDelegation.ps1
+++ Set-MailboxDelegation/Set-MailboxDelegation.ps1
@@ -1,4 +1,4 @@
-﻿#1/26/2018
+﻿#5/16/2018
 
 function Set-MailboxDelegation {
 
@@ -15,28 +15,70 @@ function Set-MailboxDelegation {
 				   Position = 1)]
 		[string[]]$delegateTo,
 		
-		[string]$pssession = $null,
+		[object]$session,
 		
 		[switch]$SendAs = $False,
 		
-		[switch]$RemovePermissions = $False,
+		[switch]$AutoMapping = $False,
 		
-		[switch]$AutoMapping = $False
+		[switch]$RemovePermissions
 	)
 	
 
 	BEGIN {
-	
+		write-verbose "[BEGIN  ] Starting: $($MyInvocation.Mycommand)"
+		$SessionResult = $null
+
+		#If a PSSession was passed, the necessary mailbox commands need to be loaded
+		if ($PSBoundParameters.ContainsKey('session')) {
+			try {
+				#Since a PSSession was explicitly passed, use -allowclobber in case the commands already exist
+				#write-verbose "[BEGIN  ] Attempting to import the necessary mailbox commands from the provided PSSession"
+				$SessionResult = Import-PSSession -Session $session -CommandName "Set-RemoteMailbox" -AllowClobber
+			} catch {
+				write-warning $_
+				throw "Unable to import the mailbox commands using the provided PSSession"
+			}
+		}
+
+		
+		#Make sure the mailbox commands are loaded
+		try {
+			#redirect the output of this command to null to supress the output
+			write-verbose "[BEGIN  ] Checking to make sure the mailbox commands are available"
+			
+			## Decide how to check this - check for all 4 commands?  In theory, as long as connected to tenant, they should all be available
+			get-command "Set-RemoteMailbox" -erroraction stop > $null
+			
+		} catch {
+			write-warning $_
+			throw "The necessary mailbox commands are not currently available. Please connect to the Exchange Tenant and try again."
+		}
+			
 	} #BEGIN
 	
 	
     PROCESS {
-	
+		foreach($user in $identity) {
+			
+			if ($PSCmdlet.ShouldProcess($user)) { 
+				
+				
+			}
+		}
+		
 	} #PROCESS
 	
 	
 	END {
-	
+		If($SessionResult -ne $null) {
+			write-verbose "[END    ] Unloading the temporary module containing the mailbox commands"
+			#Unload the temp module
+			Remove-Module $SessionResult
+		}	
+		
+		write-verbose "[END    ] Ending: $($MyInvocation.Mycommand)"
+		
 	} #END
 
 }
