7deab9e85befbefc42c237da6a15e109a1e03b1f
David Neal <35005080+davidpneal@users.noreply.github.com>
2018-12-25 20:25:51 -0600

    CryptoBlocker - Begin Implementation
    Change Get-EventLog to Get-WinEnent - the newer cmdlet is much faster and can query for a specific event ID
    Implement code to extract the username and logs generated by the user in the last minute

--- CryptoBlocker.ps1
+++ CryptoBlocker.ps1
@@ -1,4 +1,4 @@
-#12-23-2018
+#12-25-2018
 #This script is designed to be used in conjunction with the crypto FSRM file screens here: https://fsrm.experiant.ca/
 #By default, it will lock out the user after 3 file violations in a minute, then unlock the account after 30 minutes
 #If the user then triggers a lockout again within an hour, the script will not set an auto unlock
@@ -10,7 +10,7 @@
 #Note, this script will add and remove deny ACE's on the share permissions; if your workflow uses share denies
 #to restrict individual user access to a share, these might inadvertently get removed
 
-#Get-WinEvent requires installing PS5 on Server 2008R2 machines
+#Get-WinEvent requires installing PowerShell 5 on Server 2008R2 machines
 
 
 ##Settings##
@@ -18,38 +18,53 @@ $to = "admin@company.com"
 $from = "alert@company.com"
 $smtpServer = "smtp.company.com"
 
-$logfile = "c:\scripts\cryptoHits.csv"
 $autoUnlockTime = 30
 
 
 
-#Need to wait for a second so the log can be written
+#Wait for 1 second so the log can be written
 sleep -seconds 1
+$logs = Get-WinEvent -FilterHashtable @{Logname='Application';ID=8215}
 
-#Get the username
-## Change: get event 8215 instead - Get-WinEvent -FilterHashtable @{Logname='Application';ID=8215}
-#$logEvents = get-eventlog -logname Application -message "*crypto*" -newest 1
-#Need to extract the username etc from the 'properties' property: $log.properties[6] = username, [0] = message with user a files - prob copy this to email
-$username = ($logEvents.message).split()[1]
-$username = $username -replace ".*\\"
-#might set the script to ignore SYSTEM since it tends to generate false positives, esp with processing windows updates
+#Get the username; it is value 6 in the poperties of the log object
+$username = ($logs | select -first 1).properties[6]
+
+#Ignore SYSTEM; it tends to generate false positives with windows updates, plus blocking it from share permissions is pointless
+###TEST THIS
+if($username.value -eq 'NT AUTHORITY\SYSTEM'){
+	exit
+}
+
+#Filter the log, retaining any entries in the past minute by the user
+###TEST THIS - need integration testing
+$time = (get-date).addminutes(-1)
+
+$lastMin = foreach($log in $logs){
+	$user = $log.properties[6]
+	
+	#Must match the user based on .value (to make them strings), comparing on the object level wont work
+	if($user.value -eq $username.value -and $log.timecreated -gt $time){
+		$log
+	}
+}
+
+
 
 
 '@ ~algorithm~
-
-Get the user of the newest log
-
-$lastMin = Count the events in the last 1 min
-$last60 = Count the events in the last 60 minutes
 
 If($lastMin > 3)
 	Lockout the user
 		#Block their share permissions - this adds a deny to the share perms but leaves NTFS alone
 		Get-SmbShare | Where-Object currentusers -gt 0 | %{Block-SmbShareAccess -Name $_.name -AccountName $username -force}
-		
+	
+	
+	#$log.properties[0] = message with info on why the event was triggered; maybe copy this to email
 	Set notification email subject to "user locked out"
 		Body of the email: a list of the files that triggered the lockout
-	
+				
+		
+	$last60 = Count the events in the last 60 minutes
 	If (unlocks in last 60m > 6)
 		Add information to email notification indicating the user will not be auto unlocked
 	Else
