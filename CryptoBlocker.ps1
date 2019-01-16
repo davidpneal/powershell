568c8a2e1cc0544a561fce5aedac423486584b72
David Neal <35005080+davidpneal@users.noreply.github.com>
2019-01-15 20:25:56 -0600

    Finish basic implementation
    Add additional components so email functionality works
    Change the number of events (was 3) and the number of minutes (was 1) to variables
    Simplify the script - remove the auto unlock feature for now

--- CryptoBlocker.ps1
+++ CryptoBlocker.ps1
@@ -1,10 +1,8 @@
-#1-3-2018
+#1/15/2019
 #This script is designed to be used in conjunction with the crypto FSRM file screens here: https://fsrm.experiant.ca/
-#By default, it will lock out the user after 3 file violations in a minute, then unlock the account after 30 minutes
-#If the user then triggers a lockout again within an hour, the script will not set an auto unlock
 
 #Set FSRM to run this script when a file screen violation is triggered
-#C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe with argument: C:\scripts\Add-CryptoBlock.ps1
+#C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe with argument: C:\scripts\CryptoBlocker.ps1
 #Set the file screen to log the event and run it as Local System
 
 #Note, this script will add and remove deny ACE's on the share permissions; if your workflow uses share denies
@@ -19,8 +17,9 @@ $lockoutNotification = "it@company.com"
 $from = "alert@company.com"
 $smtpServer = "smtp.company.com"
 
-#$autoUnlockTime = 30
-
+#Default lockout parameters: lock out the user after 3 ($numEvents) file violations in 1 minute ($numMinutes)
+$numEvents = 3
+$numMinutes = 1
 
 
 #Wait for 1 second so the log can be written
@@ -36,55 +35,50 @@ if($username -eq 'NT AUTHORITY\SYSTEM'){
 }
 
 
-#Filter the log, retaining any entries in the past minute by the user
-$time = (get-date).addminutes(-1)
+#Filter the log, retaining any entries in the past $numMinutes by the user
+$time = (get-date).addminutes(-$numMinutes)
 
 $lastMin = foreach($log in $logs){
 	$user = $log.properties[6]
 	
+	#Match entries in the past $numMinutes that were caused by $username
 	if($user.value -eq $username -and $log.timecreated -gt $time){
 		$log
 	}
 }
 
 
-if(($lastMin | measure).count -gt 3) {
-	$hostname = hostname
-	$timestamp = ($logs | select -first 1).timecreated
-	
+$hostname = hostname
+$timestamp = ($logs | select -first 1).timecreated
+
+if(($lastMin | measure).count -gt $numEvents) {
 	#Block the user's share permissions - this adds a deny to the share perms but leaves NTFS alone
 	#This is done for speed and so every child's permissions dont need to be changed
 	Get-SmbShare | Where-Object currentusers -gt 0 | foreach-object{
 		Block-SmbShareAccess -Name $_.name -AccountName $username -force
 	}
 	
-	
+	$to = $lockoutNotification
 	$subject = "$hostname CryptoLocker protection: $($username) has been locked out"
 	
 	$message = "Most recent event time stamp: $timestamp`n"
 	$message += "User: $($username)`n`n"
+	$message += "These files matched the suspect extensions and triggered the lockout:`n"	
 	
-	
-	#$log.properties[0] = full message with info on why the event was triggered; maybe copy this to email
-	
-	##(WIP) - need a for loop to do this:
-	#Body of the email: a list of the files that triggered the lockout
-	$message += 		
-	#File names: $log.properties[0].value.split()[5]
+	#Generate a list of the files that triggered the lockout
+	foreach($log in $lastMin) {
+		$message += $log.properties[0].value.split()[5]	
+	}
 		
-		
-##Decide if want to implement this section:	
-#	$last60 = Count the events in the last 60 minutes
-#	If (unlocks in last 60m > 6)
-#		Add information to email notification indicating the user will not be auto unlocked
-#	Else
-#		Add a scheduled task to unlock the user after $autoUnlockTime minutes
-
-
-
 } else {
 	#Send a warning the script was triggered but the lockout threshold was not reached
-
+	$to = $warningNotification
+	$subject = "$hostname CryptoLocker warning triggered for: $($username)"
+	
+	$message = "$($username) triggered a warning but has not been locked out`n"
+	$message += "Most recent event time stamp: $timestamp`n`n"
+	#Full message with info on why the event was triggered
+	$message += ($logs | select -first 1).properties[0]
 	
 }
 
@@ -93,19 +87,7 @@ Send-MailMessage -SmtpServer $smtpServer -from $from -to $to -subject $subject -
 
 
 
-----------------------------------------------------------------------
-
-
-#Dec 2018 - TEST this ~ still applies???
+#TEST this ~ still applies???
 #FSRM must be restarted after being triggered - it wont run twice for some reason
-restart-service "File Server Resource Manager" -force
+#restart-service "File Server Resource Manager" -force
 
-
-@'
-Testing hack - can use multiple variable assignment to drop off the first element(s) from the logs array
-based on the principle of: $a, $b, $c = 1, 2, 3 -- gives a = 1, b = 2, c = 3
->If there are more items on the right side of the evaluation than on the left, the remaining elements are
-assigned to the last element, so this will drop the first element (it gets assigned to $null):
-$null, $logs = $logs
-
-'@
